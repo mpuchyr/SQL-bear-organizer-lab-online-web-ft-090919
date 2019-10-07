5be5f4902fbded47f215da9823e603341e99ce8b
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-07 21:15:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,8 +19,8 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT * FROM bears ORDER BY COUNT(color) DESC LIMIT 1;"
-end
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
+end 
 
 def counts_number_of_bears_with_goofy_temperaments
   "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
