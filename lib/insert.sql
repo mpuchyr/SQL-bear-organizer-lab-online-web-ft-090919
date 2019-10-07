5fce763276e0447ca9f42edac571ed76c5dc3a80
Matt Puchyr <matt.puchyr@gmail.com>
2019-10-07 20:15:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "irritable", 1);
